e3da8f0990cdff246bcd8f55ca629d5a303b4887
Triceratops228 <t-gallyamov@bk.ru>
2023-12-28 00:33:24 +0300

    final

--- CPD/Task_4/client.py
+++ CPD/Task_4/client.py
@@ -3,23 +3,27 @@ import socket
 from PySide6.QtCore import *
 from PySide6.QtNetwork import *
 import video_pb2
+import struct
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('localhost', 5252)
+# Подключаем сокет к порту, через который прослушивается сервер
+server_address = ('localhost', 5050)
+print('Подключение к {} порт {}'.format(*server_address))
+
 
 sock.connect(server_address)
 print('Подключено')
-video = cv2.VideoCapture(0)
+vid = cv2.VideoCapture(0)
 message = video_pb2.Video()
-
-while True:   
-    _, frame = video.read()
-    pressed_frame = cv2.resize(frame, (144, 144), interpolation=cv2.INTER_AREA)
-    _, img_encoded = cv2.imencode('.jpg', pressed_frame)
-    data = img_encoded.tobytes()
-    message.video_data = data
+while True:
+    ret, frame = vid.read()
+    #compressed_frame = cv2.resize(frame, (176, 144), interpolation=cv2.INTER_AREA)
+    _, img_encoded = cv2.imencode('.jpg', frame)
+    string_data = img_encoded.tobytes()
+    message.video_data = string_data
     serialized_message = message.SerializeToString()
     print(len(serialized_message))
-    sock.sendall(serialized_message)
+    message1 = len(serialized_message).to_bytes(4, byteorder='big') + serialized_message
+    sock.sendall(message1)
     cv2.imshow('client', frame)
 vid.release()
--- CPD/Task_4/server.py
+++ CPD/Task_4/server.py
@@ -3,30 +3,49 @@ from tkinter import Label
 from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
 from PySide6.QtGui import QPixmap
 from PySide6.QtNetwork import QTcpServer, QTcpSocket, QHostAddress
-import video_pb2 
+import video_pb2  # Импортируем сгенерированный protobuf-класс
+import cv2
+import struct
+import io
+
 class VideoServer():
     def __init__(self):
         self.server = QTcpServer()
+        if not self.server.listen(address=QHostAddress.Any, port=5050):
+            print("Unable to start the server: %s." % self.server.errorString())
+        print('Server started')
         self.message = video_pb2.Video()
-        self.server.newConnection.connect(self.connect)
+        self.buffer = io.BytesIO()
+        self.server.newConnection.connect(self.new_connection)
         self.mainWin = QMainWindow()
         self. lbl = QLabel()
         self.mainWin.setCentralWidget(self.lbl)
         self.lbl.show()
         self.mainWin.show()
-        print('Server started')
 
-    def connect(self):
+    def new_connection(self):
         self.connection = self.server.nextPendingConnection()
-        self.connection.readyRead.connect(self.show_frames)
+        self.connection.readyRead.connect(self.process_frame)
         print(f'Connected to {self.connection}')
 
-    def show_frames(self):
-        frame_data = self.connection.readAll().data()
-        self.message.ParseFromString(frame_data)
-        pixmap = QPixmap()
-        pixmap.loadFromData(self.message.video_data)
-        self.lbl.setPixmap(pixmap)
+    def process_frame(self):
+        self.buffer.write(self.connection.readAll())
+        data_size = len(self.buffer.getvalue())
+        if data_size >= 4:
+            # Получение размера сообщения из первых 4 байт
+            message_size = int.from_bytes(self.buffer.getvalue()[:4], 'big')
+            if data_size >= 4 + message_size:
+                # Извлечение данных видео и их обработка
+                self.message.ParseFromString(self.buffer.getvalue()[4:4 + message_size])
+                pixmap = QPixmap()
+                pixmap.loadFromData(self.message.video_data)
+                self.lbl.setPixmap(pixmap)
+
+                # Обрезаем буфер после успешной обработки сообщения
+                self.buffer = io.BytesIO(self.buffer.getvalue()[4 + message_size:])
+
+        # Получаем видео данные и выполняем необходимую обработкa
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
